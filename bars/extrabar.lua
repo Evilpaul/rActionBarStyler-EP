c7402c507c3ee15ec7bfd0eef7e9db5d209a9901
Evilpaul <yj589794@googlemail.com>
2012-01-29 03:21:51 +0000

    fix the extra bar

--- bars/extrabar.lua
+++ bars/extrabar.lua
@@ -6,72 +6,45 @@ local cfg = ns.cfg
 local barcfg = cfg.bars.extrabar
 
 if not barcfg.disable then
-
-	local num = 1
-
 	local bar = CreateFrame('Frame', 'rABS_ExtraActionBar', UIParent, 'SecureHandlerStateTemplate')
-	bar:SetWidth(barcfg.buttonsize * num + barcfg.buttonspacing * (num - 1))
-	bar:SetHeight(barcfg.buttonsize)
+	bar:SetSize(barcfg.buttonsize, barcfg.buttonsize)
 	bar:SetPoint(barcfg.pos.a1, barcfg.pos.af, barcfg.pos.a2, barcfg.pos.x, barcfg.pos.y)
-
-	if barcfg.testmode then
-		bar:SetBackdrop(cfg.backdrop)
-		bar:SetBackdropColor(1, 0.8, 1, 0.6)
-	end
 	bar:SetScale(barcfg.barscale)
 
-	ExtraActionBarFrame:SetParent(bar)
-	ExtraActionBarFrame:EnableMouse(false)
+	-- the frame
+	local f = ExtraActionBarFrame
+	f:SetParent(bar)
+	f:ClearAllPoints()
+	f:SetPoint('CENTER', 0, 0)
+	f.ignoreFramePositionManager = true
 
+	-- the button
+	local b = ExtraActionButton1
+	b:SetSize(barcfg.buttonsize, barcfg.buttonsize)
+	bar.button = b
+
+	-- style texture
+	local s = b.style
+	s:SetTexture(nil)
 	local disableTexture = function(style, texture)
+		if not texture then
+			return
+		end
 		if string.sub(texture, 1, 9) == 'Interface' then
-			style:SetTexture('')--disable button style texture
+			style:SetTexture(nil) --bzzzzzzzz
 		end
 	end
+	hooksecurefunc(s, 'SetTexture', disableTexture)
 
-	for i = 1, num do
-		local button = _G['ExtraActionButton' .. i]
-		if not button then return end
-		if button.style then
-			button.style:SetTexture('')
-			hooksecurefunc(button.style, 'SetTexture', disableTexture)
-		end
-		button:SetSize(barcfg.buttonsize, barcfg.buttonsize)
-		button:ClearAllPoints()
-		if i == 1 then
-			button:SetPoint('BOTTOMLEFT', bar, 0,0)
+	-- register the event, make sure the damn button shows up
+	bar:RegisterEvent('UPDATE_EXTRA_ACTIONBAR')
+	bar:RegisterEvent('PLAYER_REGEN_ENABLED')
+	bar:SetScript('OnEvent', function(self, event, ...)
+		if(HasExtraActionBar()) then
+			self:Show()
+			self.button:Show()
 		else
-			local previous = _G['ExtraActionButton' .. i - 1]
-			button:SetPoint('LEFT', previous, 'RIGHT', barcfg.buttonspacing, 0)
+			self:Hide()
 		end
-	end
-
-	if barcfg.showonmouseover then
-		local function lighton(alpha)
-			if ExtraActionBarFrame:IsShown() then
-				for i = 1, num do
-					local eb = _G['ExtraActionButton' .. i]
-					eb:SetAlpha(alpha)
-				end
-			end
-		end
-		bar:EnableMouse(true)
-		bar:SetScript('OnEnter', function(self)
-			lighton(1)
-		end)
-		bar:SetScript('OnLeave', function(self)
-			lighton(0)
-		end)
-		for i = 1, num do
-			local eb = _G['ExtraActionButton' .. i]
-			eb:SetAlpha(0)
-			eb:HookScript('OnEnter', function(self)
-				lighton(1)
-			end)
-			eb:HookScript('OnLeave', function(self)
-				lighton(0)
-			end)
-		end
-	end
-
+	end)
 end --disable
